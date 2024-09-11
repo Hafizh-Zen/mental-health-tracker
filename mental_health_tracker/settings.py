46512bb55b5b2c8036be0c4bfe30b1b4031f378f
Hafizh Zen <hafizh.suryamustafa@gmail.com>
2024-09-11 12:12:00 +0700

    pws url

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -123,4 +123,4 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://https://hafizh-surya-mentalhealthtracker.pbp.cs.ui.ac.id/", "https://https://hafizh-surya-mentalhealthtracker.pbp.cs.ui.ac.id/"]
+CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","https://hafizh-surya-mentalhealthtracker.pbp.cs.ui.ac.id/", "https://hafizh-surya-mentalhealthtracker.pbp.cs.ui.ac.id/"]
