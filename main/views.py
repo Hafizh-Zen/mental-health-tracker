adb117db792cd7afabcd1853dfc7fec6a147f7ce
Hafizh Zen <hafizh.suryamustafa@gmail.com>
2024-09-25 11:44:28 +0700

    tutorial 4

--- main/views.py
+++ main/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.shortcuts import render, redirect   # Add import redirect at this line
+from django.shortcuts import render, redirect, reverse   # Add import redirect at this line
 from main.forms import MoodEntryForm
 from main.models import MoodEntry
 from django.http import HttpResponse
@@ -11,7 +11,7 @@ from django.contrib.auth import authenticate, login
 from django.contrib.auth import logout
 from django.contrib.auth.decorators import login_required
 import datetime
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponseRedirect
 from django.urls import reverse
 
 # Create your views here.
@@ -100,3 +100,26 @@ def logout_user(request):
     response = HttpResponseRedirect(reverse('main:login'))
     response.delete_cookie('last_login')
     return response
+
+def edit_mood(request, id):
+    # Get mood entry based on id
+    mood = MoodEntry.objects.get(pk = id)
+
+    # Set mood entry as an instance of the form
+    form = MoodEntryForm(request.POST or None, instance=mood)
+
+    if form.is_valid() and request.method == "POST":
+        # Save form and return to home page
+        form.save()
+        return HttpResponseRedirect(reverse('main:show_main'))
+
+    context = {'form': form}
+    return render(request, "edit_mood.html", context)
+
+def delete_mood(request, id):
+    # Get mood based on id
+    mood = MoodEntry.objects.get(pk = id)
+    # Delete mood
+    mood.delete()
+    # Return to home page
+    return HttpResponseRedirect(reverse('main:show_main'))
